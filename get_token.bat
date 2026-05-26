9c03f9974cd338722b596ad99f31e7781dcd505d
Too-ETGY <perfectworld3333@gmail.com>
2026-05-26 19:46:24 +0700

    sakaboom

--- get_token.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Getting authentication token...
-
-curl -X POST ^
-  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDHrZhP4XINErE4CBjCGA25WXiojNt798s" ^
-  -H "Content-Type: application/json" ^
-  -d "{\"email\":\"test@test.com\",\"password\":\"Test1234!\",\"returnSecureToken\":true}"
